5be17a6aadc4a285341d57724b43b7431d43e739
Muhammad Ishrath <ishrathmuhammadu@gmail.com>
2024-10-19 00:07:55 +0530

    cerated an UI to delete a vehicle by ID

--- src/main/java/lk/aulakapora/vehiculohub/HelloApplication.java
+++ src/main/java/lk/aulakapora/vehiculohub/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("create-vehicle-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("delete-vehicle-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 300);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
