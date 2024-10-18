d32b0f005aedb4597d566889fb2d1427000c87f5
Muhammad Ishrath <ishrathmuhammadu@gmail.com>
2024-10-19 00:39:14 +0530

    created a vehiculo_hub database and vehicle table

---
+++ src/db.sql
@@ -0,0 +1,21 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS vehiculohub;
+
+-- Use the database
+USE vehiculohub;
+
+-- Create the vehicle table
+CREATE TABLE vehicle
+(
+    vid              INT AUTO_INCREMENT PRIMARY KEY,
+    brand            VARCHAR(50),
+    model            VARCHAR(50),
+    engineCapacity   INT,
+    transmissionMode VARCHAR(50),
+    qty              INT,
+    price            DECIMAL(10, 2)
+);
+
+
+
+
